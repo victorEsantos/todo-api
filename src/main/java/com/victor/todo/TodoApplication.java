5fdfdf16b20b62b7f4e84dd87366cdce4cfc7b9e
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-08-27 20:11:35 -0300

    Initial commit

---
+++ src/main/java/com/victor/todo/TodoApplication.java
@@ -0,0 +1,34 @@
+package com.victor.todo;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import com.victor.todo.domain.Todo;
+import com.victor.todo.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Todo t1 = new Todo(null, "Estudar Spring", LocalDateTime.now(), null, null);
+		Todo t2 = new Todo(null, "Estudar Angular", LocalDateTime.now(), null, null);
+		Todo t3 = new Todo(null, "Deploy do backend no Heroku", LocalDateTime.now(), null, null);
+		Todo t4 = new Todo(null, "Angular no Github pages", LocalDateTime.now(), null, null);
+		
+		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+	}
+
+}
---
+++ src/main/java/com/victor/todo/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.victor.todo.repositories;
+
+import com.victor.todo.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+
+}
